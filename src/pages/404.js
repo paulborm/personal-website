8b82e61bfd3e2b8fe3e909e870bf4d8ca76e2afc
Paul Borm <mail@paulborm.de>
2019-09-28 17:35:07 +0200

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,36 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Headline from "../components/Headline"
 
 const NotFoundPage = () => (
-    <Layout>
+    <Layout style={{
+        display: `flex`,
+        flex: `1 1 auto`,
+        alignItems: `center`
+    }}>
         <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Headline
+            look="large"
+            style={{
+                margin: `0 auto`,
+                maxWidth: `16em`,
+                fontFamily: `monospace`,
+                fontWeight: `normal`,
+            }}
+        >
+            <span
+                role="img"
+                aria-label="Working guy in front of a notebook"
+                style={{
+                    userSelect: `none`,
+                    pointerEvents: `none`
+                }}
+            >
+                🤦‍♂️
+            </span>
+            <br/> <span style={{ textDecoration: `line-through` }}>Keine Seite gefunden.</span>
+        </Headline>
     </Layout>
 )
 
