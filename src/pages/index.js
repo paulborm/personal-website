0ee2703a641a3f277aa5c7c22903340d6ebc6ac8
Paul Borm <mail@paulborm.de>
2020-04-13 18:43:15 +0200

    Update wording and formatting in gatsby-config.js and pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,51 +5,56 @@ import SEO from "../components/seo"
 import Headline from "../components/Headline"
 
 const IndexPage = () => (
-    <Layout style={{
-        display: `flex`,
-        flex: `1 1 auto`,
-        alignItems: `center`
-    }}>
-        <style>
-            {`@keyframes fadeUp {
-                from {
-                    opacity: 0;
-                    transform: translateY(40%);
-                }
-                to {
-                    opacity: 1;
-                    transform: translateY(0);
-                }
-            }`}
-        </style>
-        <SEO title="Frontend-Developer & Mediengestalter | Stuttgart, Esslingen, Wendlingen" />
-        <Headline
-            as="h2"
-            look="large"
-            style={{
-                margin: `0 auto`,
-                maxWidth: `16em`,
-                fontFamily: `monospace`,
-                fontWeight: `normal`,
-                animationName: `fadeUp`,
-                animationDuration: `1000ms`,
-                animationDelay: `350ms`,
-                animationFillMode: `both`
-            }}
-        >
-            <span
-                role="img"
-                aria-label="Working guy in front of a notebook"
-                style={{
-                    userSelect: `none`,
-                    pointerEvents: `none`
-                }}
-            >
-                👨‍💻
-            </span>
-            <br/>Frontend-Developer & Mediengestalter aus dem Raum Stuttgart.
-        </Headline>
-    </Layout>
+  <Layout
+    style={{
+      display: `flex`,
+      flex: `1 1 auto`,
+      alignItems: `center`,
+    }}
+  >
+    <style>
+      {`
+        @keyframes fadeUp {
+            from {
+                opacity: 0;
+                transform: translateY(40%);
+            }
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+    `}
+    </style>
+    <SEO title="Frontend-Entwickler & Mediengestalter | Stuttgart, Esslingen, Wendlingen" />
+    <Headline
+      as="h2"
+      look="large"
+      style={{
+        margin: `0 auto`,
+        maxWidth: `16em`,
+        fontFamily: `monospace`,
+        fontWeight: `normal`,
+        animationName: `fadeUp`,
+        animationDuration: `1000ms`,
+        animationDelay: `350ms`,
+        animationFillMode: `both`,
+      }}
+    >
+      <span
+        role="img"
+        aria-label="Working guy in front of a notebook"
+        style={{
+          userSelect: `none`,
+          pointerEvents: `none`,
+        }}
+      >
+        👨‍💻
+      </span>
+      <br />
+      Frontend-Entwickler & Mediengestalter aus dem Raum Stuttgart.
+    </Headline>
+  </Layout>
 )
 
 export default IndexPage
